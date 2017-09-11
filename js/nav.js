61a8969323ca3f10e2995610568e810d4f24fa40
Kunal Mishra <kunmishra2599@gmail.com>
2017-09-11 15:21:37 +0800

    Added nav bottom border, replaced logos in nav with lil images instead

--- js/nav.js
+++ js/nav.js
@@ -5,12 +5,14 @@ $(document).ready(function() {
       $(".navbar").css("background", "white");
       $(".navbar-light .navbar-nav .nav-link").css("color", "rgba(0,0,0,0.5)");
       $(".navbar-light .navbar-nav .active>.nav-link").css("color", "black");
-      $("#nav-logo").attr("src","../assets/logo/Logo_Colour.png");
+      $("#nav-logo").attr("src","../assets/logo/Logo_Colour_Small.png");
+      $(".navbar").css("border-bottom", "1px grey solid");
     } else {
       $(".navbar").css("background", "transparent");
-      $("#nav-logo").attr("src","../assets/logo/Logo_Light.png");
+      $("#nav-logo").attr("src","../assets/logo/Logo_Light_Small.png");
       $(".navbar-light .navbar-nav .nav-link").css("color", "rgba(255,255,255,0.5)");
       $(".navbar-light .navbar-nav .active>.nav-link").css("color", "white");
+      $(".navbar").css("border-bottom", "1px white none");
     }
   });
 });
