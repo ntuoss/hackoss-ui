b1956ca6e94816b132d5267fa0c1647dd748ac25
Kunal Mishra <kunmishra2599@gmail.com>
2017-09-30 10:55:38 +0800

    Changed footer, added some images, fixed some responsiveness :)))

--- js/lil-nav.js
+++ js/lil-nav.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
   $(window).on("scroll", function() {
     var wn = $(window).scrollTop();
-    if (wn > 75) {
-      $(".navbar").css("background", "white");
-      $(".navbar-light .navbar-nav .nav-link").css("color", "rgba(0,0,0,0.5)");
-      $(".navbar-light .navbar-nav .active>.nav-link").css("color", "black");
-      $("#nav-logo").attr("src","../assets/logo/Logo_Colour_Small.png");
-      $(".navbar").css("border-bottom", "1px grey solid");
-    } else {
-      $(".navbar").css("background", "transparent");
-      $("#nav-logo").attr("src","../assets/logo/Logo_Light_Small.png");
-      $(".navbar-light .navbar-nav .nav-link").css("color", "rgba(255,255,255,0.5)");
-      $(".navbar-light .navbar-nav .active>.nav-link").css("color", "white");
-      $(".navbar").css("border-bottom", "1px white none");
-    }
+
+    const mq = window.matchMedia( "(min-width: 991px)" );
+    if (mq.matches) {
+      if (wn > 75) {
+        $(".navbar").css("background", "white");
+        $(".navbar-light .navbar-nav .nav-link").css("color", "rgba(0,0,0,0.5)");
+        $(".navbar-light .navbar-nav .active>.nav-link").css("color", "black");
+        $("#nav-logo").attr("src","../assets/logo/Logo_Colour_Small.png");
+        $(".navbar").css("border-bottom", "1px grey solid");
+      } else {
+        $(".navbar").css("background", "transparent");
+        $("#nav-logo").attr("src","../assets/logo/Logo_Light_Small.png");
+        $(".navbar-light .navbar-nav .nav-link").css("color", "rgba(255,255,255,0.5)");
+        $(".navbar-light .navbar-nav .active>.nav-link").css("color", "white");
+        $(".navbar").css("border-bottom", "1px white none");
+      }
+      } else {
+          $(".navbar").css("background", "white");
+          $(".navbar-light .navbar-nav .nav-link").css("color", "rgba(0,0,0,0.5)");
+          $(".navbar-light .navbar-nav .active>.nav-link").css("color", "black");
+          $("#nav-logo").attr("src","../assets/logo/Logo_Colour_Small.png");
+          $(".navbar").css("border-bottom", "1px grey solid");
+          $(".navbar-collapse").css("background", "white");
+      }
   });
 });
