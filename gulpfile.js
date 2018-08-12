2cd4463314d4ca944b6ef7570df32c30324d40d5
Kunal Mishra <kunmishra2599@gmail.com>
2018-08-12 15:15:58 +0530

    Added buttons for joining

--- gulpfile.js
+++ gulpfile.js
@@ -11,39 +11,58 @@ var babel = require('gulp-babel');
 
 // Init BrowserSync
 gulp.task('browserSync', function() {
-    browserSync.init({ server: { baseDir: '' } });
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+    port: 8080,
+    open: true,
+    notify: false
+  });
 });
 
 // Compile Sass
 gulp.task('sass', function() {
-    return gulp.src('css/scss/main.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(rename('custom.min.css'))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({ stream: true }));
+  return gulp.src('css/scss/main.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(rename('custom.min.css'))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // Lint JS
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
-        .pipe(jshint({ esversion: 6 }))
-        .pipe(jshint.reporter('default'));
+  return gulp.src('js/*.js')
+    .pipe(jshint({
+      esversion: 6
+    }))
+    .pipe(jshint.reporter('default'));
 });
 
 // Minify JS
 gulp.task('minify', function() {
-    return gulp.src('js/*.js')
-        .pipe(babel({ presets: ['es2015'] }))
-        .pipe(uglify().on('error', function(err) { console.log(err.toString()); }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js/min'));
+  return gulp.src('js/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify().on('error', function(err) {
+      console.log(err.toString());
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('js/min'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', ['browserSync', 'sass', 'lint', 'minify'], function() {
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('css/scss/**/*.scss', ['sass', browserSync.reload]);
-    gulp.watch('js/*.js', ['lint', 'minify', browserSync.reload]);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('css/scss/**/*.scss', ['sass', browserSync.reload]);
+  gulp.watch('js/*.js', ['lint', 'minify', browserSync.reload]);
 });
 
 // Default Task
