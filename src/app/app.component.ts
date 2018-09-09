2db616de82be4df145a951749be092cb3e443ce5
Clarence Castillo <clarencecastillo@outlook.com>
2018-09-09 11:55:36 +0800

    Implement Google Calendar API proof of concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalendarService } from './services/calendar/calendar.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private calendarService: CalendarService) { }
+
+  async getEvents() {
+    console.log(await this.calendarService.getEvents());
+  }
 }
