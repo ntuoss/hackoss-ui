469d504b8b962f3dcf461581a1e80a9ebe22e619
Li Mengyang <LIME0040@e.ntu.edu.sg>
2018-09-13 16:59:18 +0800

    I added the facebook photos token here

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  googleApiUrl: 'www.googleapis.com',
-  googleCalendarId: 'ntuoss@gmail.com',
-  googleApiKey: 'AIzaSyDWzoB1A_zTDM9eVClHKkwR5IT3UOVeats',
-  eventbriteToken: 'JH5YG6RWF7BDGBLRGPBJ',
-  eventbriteApiUrl: 'www.eventbriteapi.com'
+    production: false,
+    googleApiUrl: 'www.googleapis.com',
+    googleCalendarId: 'ntuoss@gmail.com',
+    googleApiKey: 'AIzaSyDWzoB1A_zTDM9eVClHKkwR5IT3UOVeats',
+    eventbriteToken: 'JH5YG6RWF7BDGBLRGPBJ',
+    eventbriteApiUrl: 'www.eventbriteapi.com'
+    facebookPhotosToken: 'EAAE3R0omZCJUBAHvuLABjVTnHiHtVQrtktFRcNB8cjscTC3hZBsx2k9urAEcbMWJeF7HVb48oDaS99glfxxSRSjUpyZCtAWyGLs3ZAqTEyYs6PKVwVEcFRWY4Fi105kgURZAMRPEltg35adsvnQnhpdKYQ0yhaIqtN6ZALJDxNl9Bn9zZCXL6Kx7p3aKm6JlY8repc4U3QHCwZDZD',
+    facebookUrl: 'https://www.facebook.com/mengyang.li.775/photos?lst=100013809856212%3A100013809856212%3A1536804807&source_ref=pb_friends_tl'
 };
 
 /*
