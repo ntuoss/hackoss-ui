f64a3ad9f2fbb771397b8d08fbf18b152df61656
Anqi Tu <anqitu@outlook.com>
2018-10-02 21:20:57 +0800

    Implement location for home

--- src/app/pages/home/location/location.component.ts
+++ src/app/pages/home/location/location.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-location',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
+  map;
+  ntuLocation = [103.680461, 1.347392];
+
   constructor() { }
 
   ngOnInit() {
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2xhcmVuY2VjYXN0aWxsbyIsImEiOiJjamtrbDQ4Y2EwaGRqM3FtZ3hmYTg0MXk0In0.GqNGJYtxVGWG0epXUtqILw';
+    const locationBound = 0.017;
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      center: this.ntuLocation,
+      zoom: 16,
+      style: 'mapbox://styles/mapbox/light-v9',
+      minZoom: 12,
+      maxZoom: 18,
+      maxBounds: new mapboxgl.LngLatBounds([this.ntuLocation[0] - locationBound, this.ntuLocation[1] - locationBound],
+        [this.ntuLocation[0] + locationBound, this.ntuLocation[1] + locationBound]),
+    });
+
+    const marker = new mapboxgl.Marker().setLngLat(this.ntuLocation).addTo(this.map);
   }
 
+  onClickBack() {
+    this.map.flyTo({
+      center: this.ntuLocation,
+      zoom: 16,
+      speed: 1,
+      curve: 1,
+    });
+  }
 }
