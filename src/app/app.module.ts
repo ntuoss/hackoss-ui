bbf1dc27e6a8a35b849f65a5ca91a15e6e29e6d2
Clarence Castillo <clarencecastillo@outlook.com>
2018-10-05 13:25:25 +0800

    Refactor map and add animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { ServicesModule } from './services/services.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     PagesModule,
--- src/app/pages/home/location/location.component.ts
+++ src/app/pages/home/location/location.component.ts
@@ -1,42 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-import mapboxgl from 'mapbox-gl';
+import * as mapboxgl from 'mapbox-gl';
+import { Map as MapboxMap, Marker, LngLatBounds } from 'mapbox-gl';
+import { environment } from '../../../../environments/environment';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
-  styleUrls: ['./location.component.scss']
+  styleUrls: ['./location.component.scss'],
+  animations: [
+    trigger('mapMoved', [
+      state('centered', style({
+        opacity: '0',
+      })),
+      state('notCentered', style({
+        opacity: '1',
+      })),
+      transition('notCentered => centered', animate('200ms')),
+      transition('* => notCentered', animate('200ms'))
+    ])
+  ]
 })
 export class LocationComponent implements OnInit {
 
-  map;
-  ntuLocation = [103.680461, 1.347392];
+  map: MapboxMap;
+  readonly LOCATION_COORDINATES: mapboxgl.LngLatLike = [103.680461, 1.347392];
+  readonly DEFAULT_ZOOM_LEVEL = 16;
+
+  mapIsCentered = true;
 
   constructor() { }
 
   ngOnInit() {
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2xhcmVuY2VjYXN0aWxsbyIsImEiOiJjamtrbDQ4Y2EwaGRqM3FtZ3hmYTg0MXk0In0.GqNGJYtxVGWG0epXUtqILw';
-    const locationBound = 0.017;
-    this.map = new mapboxgl.Map({
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+
+    const locationBound = 0.02;
+    this.map = new MapboxMap({
       container: 'map',
-      center: this.ntuLocation,
-      zoom: 16,
       style: 'mapbox://styles/mapbox/light-v9',
+      center: this.LOCATION_COORDINATES,
+      zoom: this.DEFAULT_ZOOM_LEVEL,
+      pitch: 0,
       minZoom: 12,
       maxZoom: 18,
-      maxBounds: new mapboxgl.LngLatBounds([this.ntuLocation[0] - locationBound, this.ntuLocation[1] - locationBound],
-        [this.ntuLocation[0] + locationBound, this.ntuLocation[1] + locationBound]),
+      maxBounds: new LngLatBounds([this.LOCATION_COORDINATES[0] - locationBound, this.LOCATION_COORDINATES[1] - locationBound],
+        [this.LOCATION_COORDINATES[0] + locationBound, this.LOCATION_COORDINATES[1] + locationBound]),
     });
 
-    const marker = new mapboxgl.Marker().setLngLat(this.ntuLocation).addTo(this.map);
+    this.map.on('move', event => {
+      if (event.originalEvent) {
+        this.onMapInteract();
+      }
+    });
+
+    const marker = new Marker().setLngLat(this.LOCATION_COORDINATES).addTo(this.map);
   }
 
-  onClickBack() {
+  private onMapInteract() {
+    this.mapIsCentered = false;
+  }
+
+  flyToLocation() {
     this.map.flyTo({
-      center: this.ntuLocation,
-      zoom: 16,
+      center: this.LOCATION_COORDINATES,
+      zoom: this.DEFAULT_ZOOM_LEVEL,
       speed: 1,
-      curve: 1,
+      curve: 1
     });
+    this.mapIsCentered = true;
   }
 }
