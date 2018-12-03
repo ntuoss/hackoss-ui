ee6e5d7f4aec068206bff6da4a47ebf729b32964
Zayn Jarvis <ZaynJarvis@gmail.com>
2018-12-03 19:58:15 +0800

    https on facebook integration

--- functions/src/facebook.ts
+++ functions/src/facebook.ts
@@ -1,5 +1,22 @@
 import { Event } from "./model/event";
+import https from "https";
 
 export const postToFacebook = (event: Event): void => {
   return null;
+  https.get(
+    "graph.facebook.com",
+    "/your-user-id/accounts",
+    "POST",
+    {
+      name: "Maggie's Test Blog",
+      category_enum: "PERSONAL_BLOG",
+      about: "Just trying the API",
+      picture: "https://your-picture.com/pic.jpg",
+      cover_photo: '{"url":"https://your-cover-picture.com/pic.jpg"}'
+    },
+    function(response) {
+      // Insert your code here
+    }
+  );
+  FB.api;
 };
