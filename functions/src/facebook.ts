4556842383e8f98b22c0c7f719741fb6b8d4b4c7
Zayn Jarvis <ZaynJarvis@gmail.com>
2018-12-04 12:02:48 +0800

    url instruction on fb and ins

--- functions/src/facebook.ts
+++ functions/src/facebook.ts
@@ -1,22 +1,7 @@
 import { Event } from "./model/event";
-import https from "https";
 
+// https://developers.facebook.com/docs/pages/getting-started
+// Follow the instruction above to create facebook app
 export const postToFacebook = (event: Event): void => {
   return null;
-  https.get(
-    "graph.facebook.com",
-    "/your-user-id/accounts",
-    "POST",
-    {
-      name: "Maggie's Test Blog",
-      category_enum: "PERSONAL_BLOG",
-      about: "Just trying the API",
-      picture: "https://your-picture.com/pic.jpg",
-      cover_photo: '{"url":"https://your-cover-picture.com/pic.jpg"}'
-    },
-    function(response) {
-      // Insert your code here
-    }
-  );
-  FB.api;
 };
--- functions/src/instagram.ts
+++ functions/src/instagram.ts
@@ -1,5 +1,7 @@
 import { Event } from "./model/event";
 
+// https://developers.facebook.com/docs/instagram-api/content-publishing/
+// For instagram implementation
 export const postToInstagram = (event: Event): void => {
   return null;
 };
