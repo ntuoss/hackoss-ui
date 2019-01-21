7ed29764e49b8dd9fe2e7750894ebca4fd0f25e6
Clarence Castillo <clarencecastillo@outlook.com>
2019-01-22 01:11:08 +0800

    Update hackoss version and add artworks and organisations services

--- src/app/services/events/events.service.ts
+++ src/app/services/events/events.service.ts
@@ -3,6 +3,8 @@ import { EventsRepository } from 'hackoss';
 import { FirebaseService } from '../firebase/firebase.service';
 import { PeopleService } from '../people/people.service';
 import { LocationsService } from '../locations/locations.service';
+import { OrganisationsService } from '../organisations/organisations.service';
+import { ArtworksService } from '../artworks/artworks.service';
 
 
 @Injectable({
@@ -10,8 +12,13 @@ import { LocationsService } from '../locations/locations.service';
 })
 export class EventsService extends EventsRepository {
 
-    constructor(firebaseService: FirebaseService, peopleService: PeopleService, locationsService: LocationsService) {
-        super(firebaseService, peopleService, locationsService);
+    constructor(
+        firebaseService: FirebaseService,
+        peopleService: PeopleService,
+        locationsService: LocationsService,
+        organisationsService: OrganisationsService,
+        artworksService: ArtworksService) {
+        super(firebaseService, peopleService, locationsService, organisationsService, artworksService);
     }
 
 }
