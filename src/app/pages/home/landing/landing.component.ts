fbad6b8c4d53555bd1fd651225708451b6cf855f
Clarence Castillo <clarencecastillo@outlook.com>
2019-01-25 23:37:43 +0800

    Change video source to firebase storage

--- src/app/pages/home/landing/landing.component.ts
+++ src/app/pages/home/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-landing',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  videoUrl: string;
+  videoUrl: string = environment.landingPageUrl;
 
   constructor() { }
 
