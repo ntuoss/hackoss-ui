34541155b669f83ab187194d9540cddec5991445
Zayn Jarvis <ZaynJarvis@gmail.com>
2019-02-04 14:28:52 +0800

    update scrolling with possible solution

--- src/app/pages/home/navigation/navigation.component.ts
+++ src/app/pages/home/navigation/navigation.component.ts
@@ -1,56 +1,58 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+	selector: 'app-navigation',
+	templateUrl: './navigation.component.html',
+	styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+	isAtTheTop = true;
+	readonly navigationLinks: NavigationLink[] = [
+		{
+			text: 'Home',
+			path: '/home',
+			fragment: 'landing'
+		},
+		{
+			text: 'About',
+			path: '/home',
+			fragment: 'about'
+		},
+		{
+			text: 'Events',
+			path: '/home',
+			fragment: 'events'
+		},
+		{
+			text: 'Location',
+			path: '/home',
+			fragment: 'location'
+		},
+		{
+			text: 'Contact',
+			path: '/home',
+			fragment: 'contact'
+		}
+	];
 
-  isAtTheTop = true;
-  readonly navigationLinks: NavigationLink[] = [
-    {
-      text: 'Home',
-      path: '/home',
-      fragment: 'landing'
-    },
-    {
-      text: 'About',
-      path: '/home',
-      fragment: 'about'
-    },
-    {
-      text: 'Events',
-      path: '/home',
-      fragment: 'events'
-    },
-    {
-      text: 'Location',
-      path: '/home',
-      fragment: 'location'
-    },
-    {
-      text: 'Contact',
-      path: '/home',
-      fragment: 'contact'
-    }
-  ];
+	constructor() {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	@HostListener('window:scroll', [])
+	onWindowScroll() {
+		const scrollOffset =
+			window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+		this.isAtTheTop = scrollOffset / window.innerHeight < 0.2;
+	}
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.isAtTheTop = (scrollOffset / window.innerHeight) < 0.2;
-  }
-
+	scroll(fragment) {
+		document.querySelector('#' + fragment).scrollIntoView({ behavior: 'smooth', block: 'start' });
+	}
 }
 
 interface NavigationLink {
-  text: string;
-  path: string;
-  fragment?: string;
+	text: string;
+	path: string;
+	fragment?: string;
 }
